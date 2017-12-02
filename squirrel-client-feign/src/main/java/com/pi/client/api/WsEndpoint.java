4f9afd262c71f645866c139bbdd839d424de0a82
lixiang <lixiang01234567890@163.com>
2017-12-02 17:26:00 +0800

    update moudle name

--- squirrel-client-feign/src/main/java/com/pi/client/api/WsEndpoint.java
+++ squirrel-client-feign/src/main/java/com/pi/client/api/WsEndpoint.java
@@ -1,8 +1,6 @@
 package com.pi.client.api;
 
 import com.pi.client.feign.ClientEurekaClient;
-import com.pi.client.usercenter.endpoint.intf.UserAccountClient;
-import com.pi.client.usercenter.endpoint.vo.UserAccountVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
