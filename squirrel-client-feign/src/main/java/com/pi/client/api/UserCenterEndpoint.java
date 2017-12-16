084d317018f12f51b078e542d5cf8e6a6b4859d4
lixiang <lixiang01234567890@163.com>
2017-12-16 21:00:59 +0800

    user center api test

--- squirrel-client-feign/src/main/java/com/pi/client/api/UserCenterEndpoint.java
+++ squirrel-client-feign/src/main/java/com/pi/client/api/UserCenterEndpoint.java
@@ -4,10 +4,13 @@ import com.pi.usercenter.endpoint.intf.UserAccountClient;
 import com.pi.usercenter.endpoint.vo.UserAccountVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class UserCenterEndpoint {
@@ -17,9 +20,8 @@ public class UserCenterEndpoint {
     private UserAccountClient userAccountClient;
 
     @GetMapping("/userAccounts")
-    public List<UserAccountVo> getAllUserAccounts() {
-        List<UserAccountVo> userAccounts = userAccountClient.getAllUserAccounts();
-        return userAccounts;
+    public ResponseEntity<List<UserAccountVo>> getAllUserAccounts(@RequestParam("ids") Set<Long> ids) {
+        return userAccountClient.getUserAccounts(ids);
     }
 
 }
